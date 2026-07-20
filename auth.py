36363cf46c20f4864e6d559f8bceff282764225f
Mamadou Diallo <alphaabdoulaye04@gmail.com>
2026-07-20 23:57:49 +0000

    Ajout route initialisation compte admin

--- auth.py
+++ auth.py
@@ -1,11 +1,11 @@
-"""
+﻿"""
 AL MUBARACK Service - Authentification admin
 """
 import re
 from functools import wraps
 from datetime import datetime, timedelta
 
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash, current_app
 from flask_login import login_user, logout_user, login_required, current_user
 
 from models import db, Admin
@@ -15,9 +15,31 @@ auth_bp = Blueprint("auth", __name__)
 MAX_TENTATIVES = 5
 
 
+@auth_bp.route("/admin/initialiser-compte")
+def initialiser_compte():
+    if Admin.query.first():
+        return "Un compte admin existe deja. Va sur /admin/connexion pour te connecter.", 200
+
+    cle = request.args.get("cle", "")
+    if cle != current_app.config["SECRET_KEY"]:
+        return "Non autorise (cle manquante ou incorrecte).", 403
+
+    admin = Admin(username="oustage")
+    admin.set_password("almubarack2026")
+    admin.doit_changer_mdp = True
+    db.session.add(admin)
+    db.session.commit()
+
+    return (
+        "Compte admin cree avec succes.<br>"
+        "Identifiant : <b>oustage</b> - Mot de passe : <b>almubarack2026</b><br>"
+        "Connecte-toi maintenant sur /admin/connexion, puis change ce mot de passe dans 'Mon compte'."
+    ), 200
+
+
 def mot_de_passe_est_fort(mot_de_passe):
     if len(mot_de_passe) < 8:
-        return False, "Le mot de passe doit contenir au moins 8 caractères."
+        return False, "Le mot de passe doit contenir au moins 8 caracteres."
     if not re.search(r"[A-Za-z]", mot_de_passe):
         return False, "Le mot de passe doit contenir au moins une lettre."
     if not re.search(r"[0-9]", mot_de_passe):
@@ -50,7 +72,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    flash("Vous avez été déconnecté.", "info")
+    flash("Vous avez ete deconnecte.", "info")
     return redirect(url_for("auth.login"))
 
 
@@ -78,7 +100,7 @@ def mon_compte():
         current_user.set_password(nouveau)
         current_user.doit_changer_mdp = False
         db.session.commit()
-        flash("Mot de passe modifié avec succès.", "success")
+        flash("Mot de passe modifie avec succes.", "success")
         return redirect(url_for("produits_admin.liste"))
 
     return render_template("mon_compte.html")
